97ac8a1629165e84ecd1eb01690cbaa232eee9f5
Shawn Presser <shawnpresser@gmail.com>
2020-02-13 13:50:21 -0800

    Minor fix

--- training/networks_stylegan2.py
+++ training/networks_stylegan2.py
@@ -500,13 +500,13 @@ def G_synthesis_stylegan2(
     for res in range(3, resolution_log2 + 1):
         with tf.variable_scope('%dx%d' % (2**res, 2**res)):
             x = block(x, res)
+            if 2**res == 64:
+                print('Adding self-attention block to generator')
+                x = non_local_block(x, "SelfAtten", use_sn=False)
             if architecture == 'skip':
                 y = upsample(y)
             if architecture == 'skip' or res == resolution_log2:
                 y = torgb(x, y, res)
-            if 2**res == 64:
-                print('Adding self-attention block to generator')
-                x = non_local_block(x, "SelfAtten", use_sn=False)
     images_out = y
 
     assert images_out.dtype == tf.as_dtype(dtype)
@@ -671,11 +671,11 @@ def D_stylegan2(
     y = images_in
     for res in range(resolution_log2, 2, -1):
         with tf.variable_scope('%dx%d' % (2**res, 2**res)):
+            if architecture == 'skip' or res == resolution_log2:
+                x = fromrgb(x, y, res)
             if 2**res == 64:
                 print('Adding self-attention block to discriminator')
                 x = non_local_block(x, "SelfAtten", use_sn=False)
-            if architecture == 'skip' or res == resolution_log2:
-                x = fromrgb(x, y, res)
             x = block(x, res)
             if architecture == 'skip':
                 y = downsample(y)
