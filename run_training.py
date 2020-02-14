5a1595c30044540b894553084f11fe1a58fe5e9c
Shawn Presser <shawnpresser@gmail.com>
2020-02-13 16:38:51 -0800

    Drop the minibatch size to 1

--- run_training.py
+++ run_training.py
@@ -53,8 +53,8 @@ def run(dataset, data_dir, result_dir, config_id, num_gpus, total_kimg, gamma, m
     train.mirror_augment = mirror_augment
     train.image_snapshot_ticks = train.network_snapshot_ticks = 10
     sched.G_lrate_base = sched.D_lrate_base = 0.002
-    sched.minibatch_size_base = 32
-    sched.minibatch_gpu_base = 4
+    sched.minibatch_size_base = 8
+    sched.minibatch_gpu_base = 1
     D_loss.gamma = 10
     metrics = [metric_defaults[x] for x in metrics]
     desc = 'stylegan2'
